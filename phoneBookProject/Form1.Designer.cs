1a1806ac76a77e5d2901c46faa0249f12cc43b05
lovee <loveeeblossomshere@gmail.com>
2024-02-13 20:26:57 +0600

    ⚒️ Create panel for registration

--- phoneBookProject/Form1.Designer.cs
+++ phoneBookProject/Form1.Designer.cs
@@ -30,6 +30,18 @@
         {
             tableLayoutPanel1 = new TableLayoutPanel();
             button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            panel1 = new Panel();
+            textBox4 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -54,15 +66,116 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(19, 34);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(209, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(19, 101);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(209, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(19, 167);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(209, 27);
+            textBox3.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox2);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(250, 341);
+            panel1.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(19, 229);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(209, 27);
+            textBox4.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(19, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 20);
+            label1.TabIndex = 6;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(19, 78);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 20);
+            label2.TabIndex = 7;
+            label2.Text = "Last name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(19, 144);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 8;
+            label3.Text = "Phone";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(19, 206);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 20);
+            label4.TabIndex = 9;
+            label4.Text = "email";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(19, 294);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(209, 34);
+            textBox5.TabIndex = 10;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(19, 271);
+            label5.Name = "label5";
+            label5.Size = new Size(50, 20);
+            label5.TabIndex = 11;
+            label5.Text = "About";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
             Controls.Add(button1);
             Controls.Add(tableLayoutPanel1);
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -70,5 +183,16 @@
 
         private TableLayoutPanel tableLayoutPanel1;
         private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Panel panel1;
+        private Label label5;
+        private TextBox textBox5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox4;
     }
 }
